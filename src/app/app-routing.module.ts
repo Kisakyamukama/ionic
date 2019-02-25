497a51b426afc4710cb9c72158b59e08b6a4d9ac
Kisakyamukama <kabalimichael03@gmail.com>
2019-02-25 15:30:29 +0300

    second adding pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'news', loadChildren: './news/news.module#NewsPageModule' },
+  { path: 'single-page', loadChildren: './single-page/single-page.module#SinglePagePageModule' }
 ];
 @NgModule({
   imports: [
